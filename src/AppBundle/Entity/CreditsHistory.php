47d7babaef0ba9b4a0fc4c104e731ab3f7d8c963
Nicolas Tricot <sho.line.pop@gmail.com>
2015-09-24 21:02:08 +0200

    Adding My profile page

---
+++ src/AppBundle/Entity/CreditsHistory.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="credits_history",indexes={@ORM\Index(name="user_id", columns={"user_id"})})
+ */
+class CreditsHistory {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $user_id;
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $nbCreditsSpent;
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $project_id;
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $pledgeDate;
+    /**
+     * @ORM\OneToOne(targetEntity="AppBundle\Entity\Project")
+     * @ORM\JoinColumn(name="project_id", referencedColumnName="id")
+     */
+    protected $project;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user_id
+     *
+     * @param string $user_id
+     * @return integer
+     */
+    public function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    /**
+     * Get user_id
+     *
+     * @return integer 
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+    
+
+    /**
+     * Set number of spent credits
+     *
+     * @param int $credits
+     * @return integer
+     */
+    public function setNbCreditsSpent($nbCreditsSpent)
+    {
+        $this->nbCreditsSpent = $nbCreditsSpent;
+
+        return $this;
+    }
+
+    /**
+     * Get how many credits spent
+     *
+     * @return integer 
+     */
+    public function getNbCreditsSpent()
+    {
+        return $this->nbCreditsSpent;
+    }
+    
+    /**
+     * Set project_id
+     *
+     * @param string $project_id
+     * @return integer
+     */
+    public function setProjectId($project_id)
+    {
+        $this->project_id = $project_id;
+
+        return $this;
+    }
+
+    /**
+     * Get project_id
+     *
+     * @return integer 
+     */
+    public function getProjectId()
+    {
+        return $this->project_id;
+    }
+    
+    /**
+     * Set pledge date
+     *
+     * @param string $pledgeDate
+     * @return datetime
+     */
+    public function setPledgeDate($pledgeDate)
+    {
+        $this->pledgeDate = $pledgeDate;
+
+        return $this;
+    }
+
+    /**
+     * Get pledge date
+     *
+     * @return datetime 
+     */
+    public function getPledgeDate()
+    {
+        return $this->pledgeDate;
+    }
+
+    public function setProject(Project $project = null)
+    {
+        $this->project = $project;
+    }
+
+    public function getProject()
+    {
+        return $this->project;
+    }
+
+}
