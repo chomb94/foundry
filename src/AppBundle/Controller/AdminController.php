e97da5df890ba4e1556066f8b77925a23f813d42
alain tiemblo <ninsuo@gmail.com>
2015-10-24 10:16:10 +0200

    moved admin stuff to a new controller

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin", name="admin")
+     * @Security("has_role('ROLE_ADMIN')")
+     * @Template()
+     */
+    public function adminAction()
+    {
+        return array();
+    }
+}
+
