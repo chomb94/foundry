abb8c5c7666397f3eed9cde1beb091e4234673a7
alain tiemblo <ninsuo@gmail.com>
2015-10-24 10:18:05 +0200

    no need for a login-check controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,18 +3,21 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Template()
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
     	$user = $this->getUser();
+
+        // ***
+        // @TODO must be optimized
         $projects = $this->get("doctrine")->getRepository("AppBundle:Project")->findAll();
 
         foreach ($projects as $oneProject) {
@@ -22,21 +25,13 @@ class DefaultController extends Controller
             $all_credits = $this->get("doctrine")->getRepository("AppBundle:CreditsHistory")->findBy(['project' => $oneProject]);
             $oneProject->setStepsAndCredits($step_list, $all_credits);
         }
+        // ***
 
-        return $this->render('default/homepage.html.twig', array(
-            //'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        return array(
             'menu_hp' => 'active',
             'projects' => $projects,
             'user' => $user,
-        ));
-    }
-
-    /**
-     * @Route("/admin")
-     */
-    public function adminAction()
-    {
-        return new Response('<html><body>Admin page!</body></html>');
+        );
     }
 }
 
