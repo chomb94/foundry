6e56b8f2ab0b3305cdd92b2d5e2a3accc0a55306
jm.charles <jmcharles@MacBook-Pro-3.local>
2015-10-29 18:07:03 +0100

    Add oldest projects in a new section + sort by end date on Home Page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,17 +16,41 @@ class DefaultController extends BaseController
     {
         $user = $this->getUser();
 
-        $projects = $this->get("doctrine")->getRepository("AppBundle:Project")->findAll();
+        // First list with only project before end date
+        $dql = "SELECT p FROM AppBundle:Project p WHERE p.endDate <= :endDate ORDER BY p.endDate DESC";
+        $projects = $this
+            ->get("doctrine")
+            ->getEntityManager()
+            ->createQuery($dql)
+            ->setParameter("endDate", date("Y-m-d H:i:s", time()))
+            ->getResult();
 
         foreach ($projects as $oneProject) {
-            $step_list = $this->get("doctrine")->getRepository("AppBundle:Step")->findBy(['project_id' => $oneProject->getId()]);
+            $step_list = $this->get("doctrine")->getRepository("AppBundle:Step")->findBy(['project_id' => $oneProject->getId()]);    
             $all_credits = $this->get("doctrine")->getRepository("AppBundle:CreditsHistory")->findBy(['project' => $oneProject]);
             $oneProject->setStepsAndCredits($step_list, $all_credits);
         }
 
+        // Old projects (date < now)
+        $dql_old = "SELECT p FROM AppBundle:Project p WHERE p.endDate > :endDate ORDER BY p.endDate ASC";
+        $old_projects = $this
+            ->get("doctrine")
+            ->getEntityManager()
+            ->createQuery($dql)
+            ->setParameter("endDate", date("Y-m-d H:i:s", time()))
+            ->getResult();
+
+        foreach ($old_projects as $oneProject) {
+            $step_list = $this->get("doctrine")->getRepository("AppBundle:Step")->findBy(['project_id' => $oneProject->getId()]);    
+            $all_credits = $this->get("doctrine")->getRepository("AppBundle:CreditsHistory")->findBy(['project' => $oneProject]);
+            $oneProject->setStepsAndCredits($step_list, $all_credits);
+        }
+
+
         return array(
             'menu_hp' => 'active',
             'projects' => $projects,
+            'old_projects' => $old_projects,
             'user' => $user,
         );
     }
