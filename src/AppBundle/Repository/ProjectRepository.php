ed09daeac86f1253cb7e957c7bd409b055618f21
jm.charles <jmcharles@MacBook-Pro-3.local>
2017-03-07 11:53:05 +0100

    Re display past and inactif projects

--- src/AppBundle/Repository/ProjectRepository.php
+++ src/AppBundle/Repository/ProjectRepository.php
@@ -137,11 +137,9 @@ class ProjectRepository extends EntityRepository
                 WHERE
                     p.family = :familyId
                     AND p.active = 1
-                    AND p.endDate > :ts
                 ORDER BY p.active DESC, p.endDate ASC, p.creationDate DESC
         ")->setParameters(array(
              'familyId' => $familyId,
-             'ts' => new \DateTime('-30 days'),
          ))->getResult();
     }
 
